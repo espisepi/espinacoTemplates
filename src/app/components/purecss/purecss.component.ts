15e5ea6fbdeab77edd75509d4cbdc9b1a10588e2
jose angel <sepinaco@gmail.com>
2020-02-04 20:58:04 +0100

    responsive website online tutorials finalizado

--- src/app/components/purecss/purecss.component.ts
+++ src/app/components/purecss/purecss.component.ts
@@ -5,6 +5,8 @@
     "node_modules/jquery/dist/jquery.min.js"
 ]
 */
+
+// toggle -> alterna la clase que tenia definida junto con la clase que le decimos en el toggle: https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_element_classlist_toggle
 import { Component, OnInit } from '@angular/core';
 declare var jQuery: any;
 
@@ -18,9 +20,23 @@ export class PurecssComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    /*(($) => {
+      $(document).ready(() => {
+        const header = document.getElementById('header');
+        header.classList.toggle('active');
+        console.log(header);
+
+      });
+    })(jQuery);*/
+  }
+
+  onClick(){
     (($) => {
       $(document).ready(() => {
-        console.log('Hello from jQuery!');
+        const header = document.getElementById('header');
+        header.classList.toggle('active');
+        console.log(header);
+
       });
     })(jQuery);
   }
